5fb8af939951db967961fa9d05671e7e295085ec
Dang Thi Quynh <quynh.dangthi2212880@hcmut.edu.vn>
2024-04-29 17:48:23 +0700

    add memphy

--- ossim_mm_tlb_hk241/src/mm-memphy.c
+++ ossim_mm_tlb_hk241/src/mm-memphy.c
@@ -6,6 +6,7 @@
 
 #include "mm.h"
 #include <stdlib.h>
+#include<stdio.h>
 
 /*
  *  MEMPHY_mv_csr - move MEMPHY cursor
@@ -40,8 +41,10 @@ int MEMPHY_seq_read(struct memphy_struct *mp, int addr, BYTE *value)
    if (!mp->rdmflg)
      return -1; /* Not compatible mode for sequential read */
 
+   //pthread_mutex_lock(&memphy_lock);
    MEMPHY_mv_csr(mp, addr);
    *value = (BYTE) mp->storage[addr];
+   //pthread_mutex_unlock(&memphy_lock);
 
    return 0;
 }
@@ -80,8 +83,10 @@ int MEMPHY_seq_write(struct memphy_struct * mp, int addr, BYTE value)
    if (!mp->rdmflg)
      return -1; /* Not compatible mode for sequential read */
 
+   //pthread_mutex_lock(&memphy_lock);
    MEMPHY_mv_csr(mp, addr);
    mp->storage[addr] = value;
+   //pthread_mutex_unlock(&memphy_lock);
 
    return 0;
 }
@@ -134,11 +139,15 @@ int MEMPHY_format(struct memphy_struct *mp, int pagesz)
        fst = newfst;
     }
 
+   //đặt used framelist là null
+    mp->used_fp_list = NULL;
+
     return 0;
 }
 
 int MEMPHY_get_freefp(struct memphy_struct *mp, int *retfpn)
-{
+{  
+   //pthread_mutex_lock(&memphy_lock);
    struct framephy_struct *fp = mp->free_fp_list;
 
    if (fp == NULL)
@@ -147,6 +156,7 @@ int MEMPHY_get_freefp(struct memphy_struct *mp, int *retfpn)
    *retfpn = fp->fpn;
    mp->free_fp_list = fp->fp_next;
 
+   //pthread_mutex_unlock(&memphy_lock);
    /* MEMPHY is iteratively used up until its exhausted
     * No garbage collector acting then it not been released
     */
@@ -160,12 +170,39 @@ int MEMPHY_dump(struct memphy_struct * mp)
     /*TODO dump memphy contnt mp->storage 
      *     for tracing the memory content
      */
-
+    //chỉ in used_fp_list 
+    //vì nội dung của mp->storage có thể quá dài và hoặc rỗng
+    printf("print corresponding MEMPHY device's used_fp_list: ");
+    if (mp == NULL) {
+      printf("NULL device\n");
+      return -1;
+    }
+    //pthread_mutex_lock(&memphy_lock);
+    struct framephy_struct * fp = mp->used_fp_list;
+    if (fp == NULL) {
+      printf("No frame has been used\n");
+     // pthread_mutex_unlock(&memphy_lock);
+      return 0;
+    }
+    printf("\n");
+    while (fp != NULL) {
+      printf("frame fpn: %d\n", fp->fpn);
+      int phyaddr = (fp->fpn << PAGING_ADDR_FPN_LOBIT);
+      for (int off = 0; off < PAGING_PAGESZ; off++) {
+         if (mp->storage[phyaddr + off] != 0)
+            printf("Off: 0x%x, data: 0x%02x\n", off, mp->storage[phyaddr + off]);
+      }
+      printf("All offset weren't listed here are all: 0x00\n");
+      fp = fp->fp_next;
+    }
+   // pthread_mutex_unlock(&memphy_lock);
+    
     return 0;
 }
 
 int MEMPHY_put_freefp(struct memphy_struct *mp, int fpn)
 {
+   //pthread_mutex_lock(&memphy_lock);
    struct framephy_struct *fp = mp->free_fp_list;
    struct framephy_struct *newnode = malloc(sizeof(struct framephy_struct));
 
@@ -173,6 +210,8 @@ int MEMPHY_put_freefp(struct memphy_struct *mp, int fpn)
    newnode->fpn = fpn;
    newnode->fp_next = fp;
    mp->free_fp_list = newnode;
+
+   //pthread_mutex_unlock(&memphy_lock);
 
    return 0;
 }
@@ -192,6 +231,8 @@ int init_memphy(struct memphy_struct *mp, int max_size, int randomflg)
 
    if (!mp->rdmflg )   /* Not Ramdom acess device, then it serial device*/
       mp->cursor = 0;
+
+   //pthread_mutex_init(&memphy_lock, NULL);
 
    return 0;
 }
