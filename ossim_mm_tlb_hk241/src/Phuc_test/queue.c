2081b34ac4fbe51c473012a2b4f6e617576e568a
Felix Chau <116711540+felixchauhp@users.noreply.github.com>
2024-05-03 12:29:43 +0700

    Update queue.c

--- ossim_mm_tlb_hk241/src/Phuc_test/queue.c
+++ ossim_mm_tlb_hk241/src/Phuc_test/queue.c
@@ -9,41 +9,36 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
         /* TODO: put a new process to queue [q] */
-        if (!q || !proc) return ;
-        if (q->size >= MAX_QUEUE_SIZE) return ;
-        p->proc[q->size] = proc ;
+        if(!q || !proc) return;
+        if(q->size >= MAX_QUEUE_SIZE) return;
+        q->proc[q->size] = proc;
         q->size++;
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
-        /* TODO: return a pcb whose prioprity is the highest
-         * in the queue [q] and remember to remove it from q
-         * */
-        if (q->size <=0)
-        {
-	        return NULL; //Queue is empty
-        }
+    if (q->size <= 0) {
+        return NULL; // Queue is empty
+    }
 
-        // Find the index of the process with the highest priority
-        int highestPriorityIndex = 0;
-        for (int i=1; i< q->size; i++)
-        {
-                if (q->proc[i]->prio < q->proc[highestPriorityIndex]->prio)
-                {
-                        highestPriorityIndex = i;
-                }
+    // Find the index of the process with the highest priority
+    int highestPriorityIndex = 0;
+    for (int i = 1; i < q->size; i++) {
+        if (q->proc[i]->prio < q->proc[highestPriorityIndex]->prio) {
+            highestPriorityIndex = i;
         }
+    }
 
-        // Store the highest priority process
-        struct pcb_t* highest_priority_proc = q->proc[highestPriorityIndex];
-        
-        // Shift the remaining process in the queue
-        for (int i= highestPriorityIndex; i <q->size-1; i++)
-        {
-                q->proc[i] = q->proc[i+1] ;
-        }
-        q->size--;
+    // Store the highest priority process
+    struct pcb_t * highest_priority_proc = q->proc[highestPriorityIndex];
 
-        return highest_priority_proc;
+    // Shift the remaining processes in the queue
+    for (int i = highestPriorityIndex; i < q->size - 1; i++) {
+        q->proc[i] = q->proc[i + 1];
+    }
+    q->size--;
+
+    return highest_priority_proc;
 }
+
+
 
